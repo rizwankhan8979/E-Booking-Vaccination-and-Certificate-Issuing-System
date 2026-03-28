2fadcf691412ea8b935586e5876966608aeccf76
Rizwan Khan <rizwankhan.officialit@gmail.com>
2026-03-28 23:07:18 +0530

    Add Vaccine field to Appointment entity

--- Vaccine-Management-Booking-System-master/src/main/java/com/example/vaccineManagement/Entity/Appointment.java
+++ Vaccine-Management-Booking-System-master/src/main/java/com/example/vaccineManagement/Entity/Appointment.java
@@ -33,8 +33,8 @@ public class Appointment {
 
     @ManyToOne
     @JoinColumn
-    private Vaccine vaccine; // ✅ ADD THIS
-
+    private Vaccine vaccine; 
+    
     @JsonIgnore
     @ManyToOne
     @JoinColumn
